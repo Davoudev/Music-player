beea1da7ea556cf2695788565d821b9bbf9a1764
David <davidchaharmehali@gmail.com>
2022-02-06 18:23:06 +0330

    Play and pause runs

---
+++ script.js
@@ -0,0 +1,50 @@
+let title = document.querySelector(".title");
+let prev = document.querySelector(".prev");
+let playpause = document.querySelector(".playpause");
+let next = document.querySelector(".next");
+let audio = document.querySelector("audio");
+
+const songlist = [
+  {
+    path: "./Musics/Ho3ein-Didgah-(Ft-EpiCure).mp3",
+    songName: "Didgah",
+  },
+  {
+    path: "./Musics/dark-netflix-theme-song.mp3",
+    songName: "dark",
+  },
+  {
+    path: "./Musics/Mohsen Chavoshi - Kojaei - Copy.mp3",
+    songName: "kojaei",
+  },
+];
+
+let song_playing = false;
+
+//play song
+function playsong() {
+  song_playing = true;
+  audio.play();
+}
+
+//puase song
+function puasesong() {
+  song_playing = false;
+  audio.pause();
+}
+
+//play or puase song on click
+playpause.addEventListener("click", () =>
+  song_playing ? puasesong() : playsong()
+);
+
+
+function loadsong(songlist){
+title.textContent = songlist.songName ; 
+audio.src = songlist.path 
+}
+
+let i = 0 
+
+
+loadsong(songlist[i])
